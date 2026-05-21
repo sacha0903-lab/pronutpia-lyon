10cd91ebdac45f5ebf6089d3ad17261a66cf0ee1
Sacha Bleines <sachableines@macbook-air.home>
2026-05-22 01:56:03 +0200

    CMS : correction syntaxe API (CommonJS)

--- api/auth.js
+++ api/auth.js
@@ -1,8 +1,8 @@
-export default function handler(req, res) {
+module.exports = function (req, res) {
   const params = new URLSearchParams({
     client_id: process.env.OAUTH_CLIENT_ID,
     scope: 'repo,user',
     state: 'github',
   });
-  res.redirect(`https://github.com/login/oauth/authorize?${params}`);
-}
+  res.redirect('https://github.com/login/oauth/authorize?' + params.toString());
+};
--- api/callback.js
+++ api/callback.js
@@ -1,5 +1,5 @@
-export default async function handler(req, res) {
-  const { code } = req.query;
+module.exports = async function (req, res) {
+  const code = req.query.code;
 
   const r = await fetch('https://github.com/login/oauth/access_token', {
     method: 'POST',
@@ -7,31 +7,28 @@ export default async function handler(req, res) {
     body: JSON.stringify({
       client_id: process.env.OAUTH_CLIENT_ID,
       client_secret: process.env.OAUTH_CLIENT_SECRET,
-      code,
+      code: code,
     }),
   });
 
-  const { access_token, error } = await r.json();
+  const data = await r.json();
+  const access_token = data.access_token;
+  const error = data.error;
 
   if (error || !access_token) {
-    res.status(401).send(`Erreur OAuth : ${error || 'token manquant'}`);
+    res.status(401).send('Erreur OAuth : ' + (error || 'token manquant'));
     return;
   }
 
-  const msg = JSON.stringify(
-    'authorization:github:success:' +
-      JSON.stringify({ token: access_token, provider: 'github' })
-  );
+  var content = JSON.stringify({ token: access_token, provider: 'github' });
+  var msg = JSON.stringify('authorization:github:success:' + content);
 
   res.setHeader('Content-Type', 'text/html');
-  res.send(`<!doctype html><html><body><script>
-(function () {
-  function onMsg(e) {
-    window.opener.postMessage(${msg}, e.origin);
-    window.removeEventListener('message', onMsg);
-  }
-  window.addEventListener('message', onMsg, false);
-  window.opener.postMessage('authorizing:github', '*');
-})();
-</script></body></html>`);
-}
+  res.send('<!doctype html><html><body><script>' +
+    '(function(){' +
+    'function onMsg(e){window.opener.postMessage(' + msg + ',e.origin);window.removeEventListener("message",onMsg);}' +
+    'window.addEventListener("message",onMsg,false);' +
+    'window.opener.postMessage("authorizing:github","*");' +
+    '})();' +
+    '<\/script></body></html>');
+};
